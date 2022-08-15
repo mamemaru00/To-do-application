0ab121fe9939f5c6a83965040e6a094a312d420b
mamemaru00 <k.shouhei0711@gmail.com>
2022-08-15 11:18:38 +0900

    データベース

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,8 +6,14 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
+
+    // フォルダーのデータを全て取得
     public function index()
     {
-        return "Hello world";
+        $folders = Folder::all();
+
+        return view('tasks/index', [
+            'folders' => $folders,
+        ]);
     }
 }
