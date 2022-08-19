88b3120877a1740268c23cf36af180282f52a01d
mamemaru00 <k.shouhei0711@gmail.com>
2022-08-19 15:20:40 +0900

    認証外観修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,11 @@
-@extends('layouts.app')
+@extends('layout')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">ログイン</div>
+                <div class="card-header"></div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
